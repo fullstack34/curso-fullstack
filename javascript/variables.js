6c7538d1835ad5de04eb22bef536e43f8a173fc4
Kelvys Moura <kelvysmoura2@gmail.com>
2024-04-06 12:06:43 -0300

    Iniciando JS

---
+++ javascript/variables.js
@@ -0,0 +1,22 @@
+
+let nomeDaVariavel = "Valor";
+let idade = 34;
+let temperatura = 35.7;
+let naoDefinido;
+let nulo = null;
+let boleano = true;
+let naoEhUmNumero = NaN;
+let numero = NaN;
+
+// Array
+let alunos = [["Meu aluno 1", 20, NaN, true]];
+
+// Object
+let turma = {
+  quantidade: 30,
+  nome: "FS34",
+  horario: {
+      inicio: "08:00",
+      fim: "12:00"
+  }
+};
