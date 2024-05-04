f186bb8127538777db232db2210ad99d3862a229
Kelvys Moura <kelvysmoura2@gmail.com>
2024-05-04 11:44:47 -0300

    Validação de fomrularios

--- javascript/DOM/form-validate/script.js
+++ javascript/DOM/form-validate/script.js
@@ -1,17 +1,44 @@
 
-let inputEmail = document.getElementById('input-email');
-let inputSenha = document.getElementById('input-senha');
-let formLogin = document.getElementById('form-login');
-let emailError = document.getElementById('email-error');
-let senhaError = document.getElementById('senha-error');
+const elements = {
+  inputEmail: document.getElementById('input-email'),
+  inputSenha: document.getElementById('input-senha'),
+  formLogin: document.getElementById('form-login')
+}
 
-formLogin.addEventListener('submit', function(event) {
+const showEmailError = (msg) => {
+  elements.emailError.textContent = msg;
+}
 
-  let emailLength = inputEmail.value.length 
-  let senhaLength = inputSenha.value.length 
+const showSenhaError = (msg) => {
+  elements.senhaError.textContent = msg;
+}
 
-  if (!emailLength || !senhaLength) {
-    alert("Email e senha precisam ser preenchidos!");
+const showError (msg, elementId) => {
+  document.getElementById(elementId).textContent = msg;
+}
+
+elements.formLogin.addEventListener('submit', (event) => {
+
+  let emailLength = elements.inputEmail.value.length;
+  let senhaLength = elements.inputSenha.value.length;
+
+  showError('', 'email-error');
+  // emailError.textContent = '';
+  showError('', 'senha-error');
+  //senhaError.textContent = '';
+
+  if(!emailLength) {
+    showError("Email é um campo obrigatorio", 'email-error');
+    // showEmailError("Email é um campo obrigatorio");
+    // emailError.textContent = "Email é um campo obrigatorio"
+    event.preventDefault();
+  } else if(emailLength < 3) {
+    showError('Email precisa ter no minimo 3 caracteres', 'email-error');
+  }
+  if (!senhaLength) {
+    showError("Senha é um campo obrigatorio", 'senha-error');
+    // showSenhaError("Senha é um campo obrigatorio");
+    // senhaError.textContent = "Senha é um campo obrigatorio"
     event.preventDefault();
   }
 
