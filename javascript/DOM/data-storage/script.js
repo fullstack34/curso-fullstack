25174ab1b045d882c24fc41ef4f3a1857bb815df
Kelvys Moura <kelvysmoura2@gmail.com>
2024-05-11 11:55:43 -0300

    Clique duplo

--- javascript/DOM/data-storage/script.js
+++ javascript/DOM/data-storage/script.js
@@ -2,29 +2,36 @@ let todoForm = document.getElementById("todo-form");
 let inputTask = document.getElementById("input-task");
 let todoList = document.getElementById('todo-list');
 
-let taskList = window.localStorage.getItem('todo-list');
+let taskList = window.localStorage.getItem('todo-list') ?? '[]';
 taskList = JSON.parse(taskList);
 
+const addTaskHtml = (value) => {
+  let li = document.createElement('li');
+  let inputCheckbox = document.createElement('input');
+  inputCheckbox.type = 'checkbox';
+
+  li.addEventListener('dblclick', event => {
+    if(!li.style.textDecoration) {
+      li.style.textDecoration = "line-through";
+    } else {
+      li.style.textDecoration = "";
+    }
+    inputCheckbox.checked = !inputCheckbox.checked;
+  });
+  
+  li.textContent = value;
+  li.prepend(inputCheckbox);
+  todoList.append(li);
+}
+
 for(let value of taskList) {
-    let li = document.createElement('li');
-    let inputCheckbox = document.createElement('input');
-    inputCheckbox.type = 'checkbox';
-    
-    li.textContent = value;
-    li.prepend(inputCheckbox);
-    todoList.append(li);
+    addTaskHtml(value);
 }
 
 todoForm.addEventListener('submit', event => {
   event.preventDefault();
 
-  let li = document.createElement('li');
-  let inputCheckbox = document.createElement('input');
-  inputCheckbox.type = 'checkbox';
-  
-  li.textContent = inputTask.value;
-  li.prepend(inputCheckbox);
-  todoList.append(li);
+  addTaskHtml(inputTask.value);
 
   taskList.push(inputTask.value);
   window.localStorage.setItem('todo-list', JSON.stringify(taskList));
