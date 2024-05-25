14e31c06dc050e950db59f1de6df6343061c0a7e
Kelvys Moura <kelvysmoura2@gmail.com>
2024-05-25 09:15:15 -0300

    Removendo função addTaskHtml

--- javascript/DOM/data-storage/script-html.js
+++ javascript/DOM/data-storage/script-html.js
@@ -9,39 +9,6 @@ const liWithCheckbox = (task) => {
   `;
 }
 
-const addTaskHtml = (task) => {
-  let li = document.createElement('li');
-  let inputCheckbox = document.createElement('input');
-  inputCheckbox.type = 'checkbox';
-  inputCheckbox.checked = task.completed
-
-  if(task.completed) {
-    li.style.textDecoration = "line-through"
-  }
-  
-  inputCheckbox.addEventListener('change', event => {
-    if(event.target.checked){
-      li.style.textDecoration = "line-through";
-    }
-    else {
-      li.style.textDecoration = "";
-    }
-  })
-
-  li.addEventListener('dblclick', event => {
-    if(!li.style.textDecoration) {
-      li.style.textDecoration = "line-through";
-    } else {
-      li.style.textDecoration = "";
-    }
-    inputCheckbox.checked = !inputCheckbox.checked;
-  });
-  
-  li.textContent = task.todo;
-  li.prepend(inputCheckbox);
-  todoList.append(li);
-}
-
 let url = 'https://dummyjson.com/todos';
 
 let request = fetch(url)
